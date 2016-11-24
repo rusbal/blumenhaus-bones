2db1042af0dc2d928c6ca9b1fe9d58dd27905aa6
Raymond Usbal <raymond@philippinedev.com>
2016-11-25 02:50:18 +0800

    email to charset=UTF-8

--- wp-content/themes/bones-less/includes/helpers/mail_helper.php
+++ wp-content/themes/bones-less/includes/helpers/mail_helper.php
@@ -21,7 +21,8 @@ function send_mail($subject, $message, $recipients = []) {
     $recipients = array_unique($recipients);
 
 	$headers  = 'MIME-Version: 1.0' . "\r\n";
-	$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+//	$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+    $headers .= 'Content-Type: text/html; charset=UTF-8' . "\r\n";
 
 	foreach ($recipients as $recipient) {
 		if(filter_var($recipient, FILTER_VALIDATE_EMAIL)) {
