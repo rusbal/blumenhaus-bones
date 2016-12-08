4d215df99b76e25ed353a4bf33346462648a664c
Raymond Usbal <raymond@philippinedev.com>
2016-12-08 09:05:26 +0800

    send order email once

--- wp-content/themes/bones-less/includes/helpers/mail_helper.php
+++ wp-content/themes/bones-less/includes/helpers/mail_helper.php
@@ -8,7 +8,6 @@ use Rsu\Settings\ThemeSettings;
 
 function send_mail($subject, $message, $recipients = []) {
 	$recipients = (array) $recipients;
-    $recipients = array_filter($recipients);
     $recipients = array_merge($recipients, ThemeSettings::orderEmailRecipients());
 
 	/**
@@ -19,13 +18,11 @@ function send_mail($subject, $message, $recipients = []) {
     $recipients = array_unique($recipients);
 
 	$headers  = 'MIME-Version: 1.0' . "\r\n";
-//	$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
     $headers .= 'Content-Type: text/html; charset=UTF-8' . "\r\n";
 
-	foreach ($recipients as $recipient) {
-		if(filter_var($recipient, FILTER_VALIDATE_EMAIL)) {
-			wp_mail( $recipient, $subject, $message, $headers );
-		}
-	}
+    $validRecipients = array_filter($recipients, function($recipient) {
+        return filter_var($recipient, FILTER_VALIDATE_EMAIL);
+    });
+    wp_mail( $validRecipients, $subject, $message, $headers );
 }
 
